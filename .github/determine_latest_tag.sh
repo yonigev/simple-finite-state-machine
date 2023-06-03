7713739003a3f40e39f6ef74b618fc935632d1f3
jonathangeva <yonigev@gmail.com>
2023-06-03 19:43:21 +0300

    Fix automatic versioning

---
+++ .github/determine_latest_tag.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+git fetch --tags
+latest_tag=$(git tag --sort=-v:refname --list "v[0-9]*" | head -n 1)
+
+echo "Latest tag is $latest_tag"
--- .github/determine_version_bump.sh
+++ .github/determine_version_bump.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-
-#pr_description=$(curl -sSL $GITHUB_API_URL/repos/$GITHUB_REPOSITORY/pulls/$PR_NUMBER | jq -r '.body')
-pr_description="[x] major"
-
-if [[ $pr_description =~ (\[x\] ?[Mm]inor) ]]; then
-  echo "minor"
-elif [[ $pr_description =~ (\[x\] ?[Mm]ajor) ]]; then
-  echo "major"
-else
-  echo "patch"
-fi
+pr_description=$1
+echo "$pr_description"
+echo "$1"
+#if [[ $pr_description =~ (\[x\] ?[Mm]inor) ]]; then
+#  echo "minor"
+#elif [[ $pr_description =~ (\[x\] ?[Mm]ajor) ]]; then
+#  echo "major"
+#else
+#  echo "patch"
+#fi
